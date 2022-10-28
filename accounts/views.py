2e2f2b21eb287400684db3e2bb1d7b3e53ec2747
Mohamud Haroon <52501278+MoHaroon777@users.noreply.github.com>
2022-10-28 15:27:10 +0100

    Lab 9.1 complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView, DetailView
+from .models import Profile
 from .forms import CustomUserCreationForm
 
 # Create your views here.
@@ -8,3 +9,14 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+class ProfileEditView(UpdateView):
+    model = Profile
+    fields = ['fav_author']
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+class ProfilePageView(DetailView):
+    model = Profile
+    template_name = 'registration/user_profile.html'
+
